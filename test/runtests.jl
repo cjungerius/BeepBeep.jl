adb1730a1470e03091dbda76715c70c9808c7599
Chris Jungerius <cjungerius@gmail.com>
2022-08-16 16:18:35 +0200

    added basic unit testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using BeepBeep
 using Test
 
 @testset "BeepBeep.jl" begin
-    # Write your tests here.
+    @test typeof(BeepBeep.sounds) <: NamedTuple
+    @test ispath(BeepBeep.sounddir)
+    @test_logs (:warn, "Out of range: Playing random sound!") beep(0)
+    @test_logs (:warn, "Out of range: Playing random sound!") beep(12)
+    @test_logs (:warn, "Cannot parse string as sound name, filepath, or url; playing random sound!") beep("foobar")
+    @test_logs (:warn, "Unknown argument(s): playing random sound!") beep(1.5)
+    @test @beep true
+    @test @beep 2 true
 end
